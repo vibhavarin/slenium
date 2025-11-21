72d5a39b70617c298da56945d64daea98fa30627
vibhavarin <vibhavari.navale77@gmail.com>
2025-11-21 14:27:18 +0530

    add numbers

--- learning.py
+++ learning.py
@@ -1,11 +1,4 @@
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-primes = [n for n in range(2, 51) if is_prime(n)]
-print(primes)
+a = 2
+b= 2
+print('a+b')
