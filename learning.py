e3b522c819af449d612de5d486916362e7d8631a
vibhavarin <vibhavari.navale77@gmail.com>
2025-11-24 13:52:43 +0530

    new change

--- learning.py
+++ learning.py
@@ -1,4 +1,27 @@
 
 a = 2
 b= 2
-print('a+b')
+print(a+b)
+###########################################
+
+a = 2
+b = 4
+
+result = a*b
+print(result)
+
+####################################################
+
+def simple_decorator(func):
+    def wrapper(*args,**kwargs):
+        print('before the function')
+        result = func(*args,*kwargs)
+        print('after the function')
+        return result
+    return wrapper()
+
+@simple_decorator
+def hallo(name):
+    print('hallo',{name})
+
+ hallo("alice")
