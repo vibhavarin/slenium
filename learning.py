fea5f6e4fda9b60ff4f17af520cb86f486627e62
vibhavarin <vibhavari.navale77@gmail.com>
2025-11-24 15:01:50 +0530

    factorial and interest

--- learning.py
+++ learning.py
@@ -24,3 +24,20 @@ def simple_decorator(func):
 def hallo(name):
     print('hallo',{name})
 hallo("alice")
+############################################################
+
+fact = 1
+## calculate factorial using for loop
+
+for i in range(1,n+1):
+    fact *= i
+print(fact)
+
+##########################################################
+
+def fun(p,t,r):
+    return(p*t*r)/100
+p,t,r = 8,6,8
+res = fun(p,t,r)
+print(res)
+
